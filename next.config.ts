063b50d872e7f2312ef2a9b94ec7aeccc9585b08
Raj-chahar <srchahar111@gmail.com>
2025-05-04 19:43:46 +0530

    live on firebase now

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Tell Next.js to produce a fully-exportable build
+  output: 'export',
+
+  // (optional) ensures every route ends with a slash → generates `about/index.html` not `about.html`
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
