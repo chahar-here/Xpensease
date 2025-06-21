881557da70d8e4da6c0a810fa0c4ba252b6cca50
Raj-chahar <srchahar111@gmail.com>
2025-06-21 19:09:41 +0530

    contact us is now responsive

--- src/app/(site)/contactus/page.tsx
+++ src/app/(site)/contactus/page.tsx
@@ -2,9 +2,8 @@
 import { Contact } from "@/app/components/contact";
 export default function Contactus() {
   return (
-    <div className="bg-neutral-950 flex h-[700px]
-    w-[100%]">
-      <div className=" w-full h-screen flex items-center justify-center">
+    <div className="bg-neutral-950 flex w-[100%]">
+      <div className=" w-full lg:my-20 my-24 flex items-center justify-center">
         <Contact/>
       </div>
     </div>
