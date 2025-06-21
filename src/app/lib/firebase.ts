af68459e2aec020813cd417042a79082c453d25e
Raj-chahar <srchahar111@gmail.com>
2025-06-21 19:22:39 +0530

    updated firebase

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Replace the following with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCKAq-N-8MPOw6kETA3L-BOUQtKaGMOlkc",
-  authDomain: "xpensease-5a520.firebaseapp.com",
-  projectId: "xpensease-5a520",
-  storageBucket: "xpensease-5a520.firebasestorage.app",
-  messagingSenderId: "845005712128",
-  appId: "1:845005712128:web:a37245939915641d1a21e5",
-  measurementId: "G-BHYPW6GW1N"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
